fa70d8801b052934f809359216e3e83bc516adcd
Jason Noh <jn6437@gmail.com>
2018-07-29 12:54:36 +1000

    style component setup

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Button } from "../styles/components/Register.styles";
 
 class Register extends Component {
-    render () {
-        return (
-            <h1>Hello World</h1>
-        )
-    }
+  state = {
+    userName: "",
+    password: ""
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  sendForm = e => {
+    //insert interaction
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div>
+        <form className="register-form">
+          <div className="lg-headtext"> User Name </div>
+          <input
+            name="userName"
+            placeholder="user name"
+            onChange={this.handleChange}
+          />
+          <div className="lg-headtext"> Password </div>
+          <input
+            name="password"
+            placeholder="Password"
+            type="password"
+            onChange={this.handleChange}
+          />
+        </form>
+        <Button onClick={this.sendForm}> Register</Button>
+      </div>
+    );
+  }
 }
 
 export default Register;
---
+++ src/styles/components/Mixins.styles.js
@@ -0,0 +1,8 @@
+import styled, { css } from "styled-components";
+
+const setButtonProps = (backGround, textColor) => css`
+  background-color: ${backGround};
+  color: ${textColor};
+`;
+
+export { setButtonProps };
